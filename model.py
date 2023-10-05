eab992d0d2ae95072835847062e1b573d8bfb944
Shashank <shashankg.2k@gmail.com>
2023-10-05 23:39:59 +0530

    Update model.py

--- model.py
+++ model.py
@@ -116,11 +116,28 @@ class MultiHeadAttentionBlock(nn.Module):
         key = key.view(key.shape[0], key.shape[1], self.h, self.d_k).transpose(1,2)
         value = value.view(value.shape[0], value.shape[1], self.h, self.d_k).transpose(1, 2)
 
-
         x, self.attention_scores = MultiHeadAttentionBlock.attention(query, key, value, mask, self.dropout) 
-
-
         x = x.transpose(1, 2).contiguous().view(x.shape[0], -1, self.h * self.d_k)
 
         return self.w_o(x)
+    
+
+class EncoderBlock(nn.Module):
+
+    def __init__(self, self_attention_block : MultiHeadAttentionBlock, fee_forward_block: FeedForwardBlock, dropout : float):
+
+        super.__init__()
+        self.self_attention_block = self_attention_block
+        self.feed_forward_block = self.feed_forward_block
+        self.residual_connections = nn.ModuleList([ResidualConnection(dropout) for _ in range(2)])
+
+    def forward(self, x, src_mask):
+        x = self.residual_connections[0](x,lambda x : self.self_attention_block(x, x, x, src_mask))
+        x = self.residual_connections[1](x, self.feed_forward_block)
+        return x
+
+class Encoder(nn.Module):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs) 
+
      
